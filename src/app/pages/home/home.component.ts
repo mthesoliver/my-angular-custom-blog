7f8cb4f544b2572ae70b369a53645778fd64fbbd
mthesoliver <m.limaoliveira96@gmail.com>
2024-02-04 03:13:21 -0300

    ajustes de estilo e hovers

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HomeModule } from './home.module';
 
 @Component({
@@ -10,6 +10,36 @@ import { HomeModule } from './home.module';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  colModel = '';
+  colModelTwo ='';
+
+  ngOnInit(): void {
+    this.verifyIsMobile()
+  }
+
+  verifyIsMobile() {
+    this.selectColMod()
+    window.addEventListener('resize', () => {
+      if (window.innerWidth <= 769) {
+        this.colModel = 'col-1';
+        this.colModelTwo = 'col-2'
+      } else if (window.innerWidth > 769) {
+        this.colModel = 'col-2';
+        this.colModelTwo = 'col-4'
+      }
+    })
+  }
+
+  selectColMod(){
+    if (window.innerWidth <= 769) {
+      this.colModel = 'col-1';
+      this.colModelTwo = 'col-2'
+    } else if (window.innerWidth > 769) {
+      this.colModel = 'col-2';
+      this.colModelTwo = 'col-4'
+    }
+  }
 }
+
--- src/app/pages/home/large-card/large-card.component.ts
+++ src/app/pages/home/large-card/large-card.component.ts
@@ -9,8 +9,8 @@ import { Component, Input } from '@angular/core';
 })
 export class LargeCardComponent {
 
-  @Input() blogPicture:string='https://www.russorizio.com/wp-content/uploads/2016/07/ef3-placeholder-image.jpg';
-  @Input() blogTitle:string ='Esse é um título';
-  @Input() blogDescription:string='Lorem ipsum dolor sit amet consectetur adipiscing elit enim mattis, odio taciti cras ridiculus feugiat netus facilisi suspendisse mi, eget montes eros metus in etiam sollicitudin sed. Aenean pellentesque ex phasellus mauris id dignissim massa orci, convallis taciti sed felis parturient euismod erat pharetra, a scelerisque pulvinar lorem ac efficitur mus.';
+  @Input() blogPicture: string = 'https://www.russorizio.com/wp-content/uploads/2016/07/ef3-placeholder-image.jpg';
+  @Input() blogTitle: string = 'Esse é um título';
+  @Input() blogDescription: string = 'Lorem ipsum dolor sit amet consectetur adipiscing elit enim mattis, odio taciti cras ridiculus feugiat netus facilisi suspendisse mi, eget montes eros metus in etiam sollicitudin sed. Aenean pellentesque ex phasellus mauris id dignissim massa orci, convallis taciti sed felis parturient euismod erat pharetra, a scelerisque pulvinar lorem ac efficitur mus.';
 
 }
